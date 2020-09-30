cf52d5d99705e334d5b05ab2b723f6fa70e7022e
H.L. Embarcadiço de Oliveira <helena.embardcadico.oliveira@hva.nl>
2020-09-30 17:24:48 +0200

    Battleship 001

--- battleship/src/app/app.module.ts
+++ battleship/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { HomepageComponent } from './mainpage/homepage/homepage.component';
+import { HeaderComponent } from './mainpage/header/header.component';
+import { NavbarComponent } from './mainpage/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainpageComponent,
+    HomepageComponent,
+    HeaderComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
