0d2b4d9f32e03d2f34b61714097a821825057c2d
H.L. Embarcadiço de Oliveira <helena.embardcadico.oliveira@hva.nl>
2020-10-01 09:45:03 +0200

    Battleship 002

--- battleship/src/app/app.module.ts
+++ battleship/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from './app.component';
-
 import { HomepageComponent } from './mainpage/homepage/homepage.component';
 import { HeaderComponent } from './mainpage/header/header.component';
 import { NavbarComponent } from './mainpage/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { GameboardComponent } from './components/gameboard/gameboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
     HomepageComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    GameboardComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- battleship/src/app/mainpage/header/header.component.ts
+++ battleship/src/app/mainpage/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  dateToday = new Date();
+  date = this.dateToday.toLocaleDateString();
 
   constructor() { }
 
