d15ac935da97b94d969c020d9c27a3e5f616108d
daniel <dtkeijser@gmail.com>
2020-10-22 13:33:24 +0200

    added register page and login page

--- battleship/src/app/components/register/register.component.ts
+++ battleship/src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
+import {forbiddenNameValidator} from "../shared/user-name.validator";
+import {PasswordValidator} from "../shared/password.validator";
+import { RegistrationService} from "../../registration.service";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private _registrationService: RegistrationService) { }
 
   ngOnInit(): void {
   }
 
+  get userName(){
+    return this.registrationForm.get('userName')
+  }
+  get userEmail(){
+    return this.registrationForm.get('userEmail')
+  }
+
+  get password(){
+    return this.registrationForm.get('password')
+  }
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(3), forbiddenNameValidator(/password/)]],
+    userEmail: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['']
+  },{ validator: PasswordValidator});
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+    // this._registrationService.register(this.registrationForm.value)
+    //   .subscribe(
+    //     response => console.log('Success!', response),
+    //     error => console.error('Error!', error)
+    //   );
+  }
 }
