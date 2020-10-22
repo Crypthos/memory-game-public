e4941231185ad6b153c044e95a0d13629bc439f0
Crypthos <kingxD107@hotmail.com>
2020-10-22 14:21:22 +0200

    formGroup import

--- battleship/src/app/app.module.ts
+++ battleship/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { RouterModule } from "@angular/router";
 
@@ -24,13 +24,14 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
     GameboardComponent,
 
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    RouterModule,
-    DragDropModule
-  ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        RouterModule,
+        DragDropModule,
+        ReactiveFormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
