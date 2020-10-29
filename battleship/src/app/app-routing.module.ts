3f71fd8471afe4efd533d73191867c1747cb1e87
daniel <dtkeijser@gmail.com>
2020-10-29 13:11:15 +0100

    Routing updated

--- battleship/src/app/app-routing.module.ts
+++ battleship/src/app/app-routing.module.ts
@@ -1,27 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent} from  './components/login/login.component'
-import { RegisterComponent } from './components/register/register.component'
-import  { GameboardComponent } from  './components/gameboard/gameboard.component'
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './components/login/login.component'
+import {RegisterComponent} from './components/register/register.component'
+import {GameboardComponent} from './components/gameboard/gameboard.component'
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'game', component: GameboardComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'game', component: GameboardComponent},
   {path: 'login', component: LoginComponent,
-    children:[{
-    path: 'register',
-      component: RegisterComponent
-    }]
-    },
+     children: [{path: 'register', component: RegisterComponent}]},
   {path: 'register', component: RegisterComponent}
-]
-
-
+];
 
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes, { useHash: true }) ],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
