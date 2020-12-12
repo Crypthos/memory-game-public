b1c523fedfb34b49b3059344b50438fb55245a18
Helena <Hdeoliveira@upcmail.nl>
2020-12-12 16:51:25 +0100

    backend version 1.4.1 Back End - Basic Authentication -almost working

--- battleship/src/app/service/data/game-data.service.ts
+++ battleship/src/app/service/data/game-data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 export class HelloGamerBean {
   constructor(public message: string){ }
@@ -19,10 +19,35 @@ export class GameDataService {
     //console.log("Excecute Hello Gamer Bean Service")
   }
 //http://localhost:8080/hello-gamer/path-variable/team08
-
   executeHelloGamerServiceWithPathVariable(name) {
-    return this.http.get<HelloGamerBean>(`http://localhost:8080/hello-gamer/path-variable/${name}`);
+    let basicAuthHeaderString = this.CreateBasicAuthenticationHttpHeader();
+
+    let headers = new HttpHeaders({
+      Authorization: basicAuthHeaderString
+      }
+
+    )
+
+    return this.http.get<HelloGamerBean>(`http://localhost:8080/hello-gamer/path-variable/${name}`,
+      {headers});
     //console.log("Excecute Hello Gamer Bean Service")
   }
 
+  CreateBasicAuthenticationHttpHeader(){
+    let username = 'Team08'
+    let password = 'dummy'
+    let basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
+    return basicAuthHeaderString;
+  }
+
+  // Access to XMLHttpRequest at
+  // 'http://localhost:8080/hello-gamer/path-variable/team08'
+  // from origin 'http://localhost:4200' has been blocked by CORS policy:
+  // No 'Access-Control-Allow-Origin' header is present on the requested resource.
+
+
+  // Access to XMLHttpRequest at
+  // 'http://localhost:8080/hello-gamer/path-variable/team08'
+  // from origin 'http://localhost:4200' has been blocked by CORS policy:
+  // Response to preflight request doesn't pass access control check: It does not have HTTP ok status.
 }
